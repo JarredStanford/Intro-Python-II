1900df5b31871314b944813533c63ff5c54aeed3
Jarred Stanford <stanj89@gmail.com>
2019-10-03 20:53:25 -0400

    mvp day 2

--- src/item.py
+++ src/item.py
@@ -1,4 +1,14 @@
+from colorama import Fore
+
 class Item:
-    def __init__(self, name, description):
+    def __init__(self, name, description=''):
         self.name = name;
-        self.description = descritpion;
+        self.description = description or ''
+    
+    def on_take(self):
+        print('You have picked up ' + Fore.RED +f'{self.name}')
+        print(Fore.WHITE + '')
+    
+    def on_drop(self):
+        print('You have dropped ' + Fore.RED +f'{self.name}')
+        print(Fore.WHITE + '')
